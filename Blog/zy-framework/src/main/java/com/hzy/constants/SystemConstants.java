8201e0df0002aaec2ab09aab819d60f0f24c927a
zxwyhzy <2791517764@qq.com>
2023-02-24 23:23:38 +0800

    添加实体类，字段更新

--- Blog/zy-framework/src/main/java/com/hzy/constants/SystemConstants.java
+++ Blog/zy-framework/src/main/java/com/hzy/constants/SystemConstants.java
@@ -36,4 +36,6 @@ public class SystemConstants {
          */
         public static final String LINK_COMMENT = "1";
 
+        public static final Object MENU ="C" ;
+        public static final Object BUTTON = "F";
 }
---
+++ Blog/zy-framework/src/main/java/com/hzy/domain/vo/AdminUserInfoVo.java
@@ -0,0 +1,27 @@
+package com.hzy.domain.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+import java.util.List;
+
+/**
+ * @title: AdminUserInfoVo
+ * @Author zxwyhzy
+ * @Date: 2023/2/24 22:50
+ * @Version 1.0
+ */
+@Data
+@Accessors(chain = true)
+@AllArgsConstructor
+@NoArgsConstructor
+public class AdminUserInfoVo {
+
+    private List<String> permissions;
+
+    private List<String> roles;
+
+    private UserInfoVo user;
+}
