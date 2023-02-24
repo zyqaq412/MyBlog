499ef9e8f53a1f29a48194e907e0dfcb68c795ca
zxwyhzy <2791517764@qq.com>
2023-02-24 23:21:30 +0800

    返回该用户的角色

---
+++ Blog/zy-framework/src/main/java/com/hzy/mapper/RoleMapper.java
@@ -0,0 +1,23 @@
+package com.hzy.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.hzy.domain.entity.Role;
+
+import java.util.List;
+
+
+/**
+ * 角色信息表(Role)表数据库访问层
+ *
+ * @author makejava
+ * @since 2023-02-24 22:47:31
+ */
+public interface RoleMapper extends BaseMapper<Role> {
+
+    /**
+     *  返回该用户的角色
+     * @param id
+     * @return
+     */
+    List<String> selectRoleKeyByUserId(Long id);
+}
