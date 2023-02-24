beba71101f9dab0dc33e68d04bb298462cced6b2
zxwyhzy <2791517764@qq.com>
2023-02-24 14:34:40 +0800

    aop实现日志记录包括：请求URL，描述信息，Http method，调用 controller 的全路径以及执行方法，请求的 IP，请求入参，出参

---
+++ Blog/zy-framework/src/main/java/com/hzy/annotion/SystemLog.java
@@ -0,0 +1,14 @@
+package com.hzy.annotion;
+
+import org.junit.jupiter.api.Test;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface SystemLog {
+    String BusinessName();
+}
