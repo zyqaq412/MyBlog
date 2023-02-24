1a14199d976c18e77edf50931b1cad123ac9a3cc
zxwyhzy <2791517764@qq.com>
2023-02-24 17:10:15 +0800

    定时任务每10分钟从redis中数据更新数据库数据

---
+++ Blog/zy-blog/src/main/java/com/hzy/job/UpdateViewCountJob.java
@@ -0,0 +1,42 @@
+package com.hzy.job;
+
+import com.hzy.domain.entity.Article;
+import com.hzy.service.ArticleService;
+import com.hzy.utils.RedisCache;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+/**
+ * @title: UpdateViewCountJob
+ * @Author zxwyhzy
+ * @Date: 2023/2/24 16:51
+ * @Version 1.0
+ */
+@Component
+public class UpdateViewCountJob {
+
+    @Autowired
+    private RedisCache redisCache;
+
+    @Autowired
+    private ArticleService articleService;
+
+    @Scheduled(cron = "* 0/10 * * * ?")
+    public void updateViewCount(){
+        //获取redis中的浏览量
+        Map<String, Integer> viewCountMap = redisCache.getCacheMap("article:viewCount");
+
+        List<Article> articles = viewCountMap.entrySet()
+                .stream()
+                .map(entry -> new Article(Long.valueOf(entry.getKey()), entry.getValue().longValue()))
+                .collect(Collectors.toList());
+        //更新到数据库中
+        articleService.updateBatchById(articles);
+
+    }
+}
