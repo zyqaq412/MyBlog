af0ca121e46a55a1bba496561aae0f7b31e6789d
zxwyhzy <2791517764@qq.com>
2023-02-24 23:22:49 +0800

    getInfo接口用户权限，用户角色，用户信息

--- Blog/zy-admin/src/main/java/com/hzy/controller/LoginController.java
+++ Blog/zy-admin/src/main/java/com/hzy/controller/LoginController.java
@@ -1,15 +1,25 @@
 package com.hzy.controller;
 
+import com.hzy.domain.LoginUser;
 import com.hzy.domain.ResponseResult;
 import com.hzy.domain.entity.User;
+import com.hzy.domain.vo.AdminUserInfoVo;
+import com.hzy.domain.vo.UserInfoVo;
 import com.hzy.enums.AppHttpCodeEnum;
 import com.hzy.exception.SystemException;
 import com.hzy.service.LoginService;
+import com.hzy.service.MenuService;
+import com.hzy.service.RoleService;
+import com.hzy.utils.BeanCopyUtils;
+import com.hzy.utils.SecurityUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * @title: LoginController
@@ -21,6 +31,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class LoginController {
     @Autowired
     private LoginService loginService;
+    @Autowired
+    private MenuService menuService;
+
+    @Autowired
+    private RoleService roleService;
 
     @PostMapping("/user/login")
     public ResponseResult login(@RequestBody User user){
@@ -29,6 +44,28 @@ public class LoginController {
             throw new SystemException(AppHttpCodeEnum.REQUIRE_USERNAME);
         }
         return loginService.login(user);
+    }
+
+    /**
+     *
+     * @return 用户权限，用户角色，用户信息
+     */
+    @GetMapping("getInfo")
+    public ResponseResult<AdminUserInfoVo> getInfo(){
+        //获取当前登录的用户
+        LoginUser loginUser = SecurityUtils.getLoginUser();
+        //根据用户id查询权限信息
+        List<String> perms = menuService.selectPermsByUserId(loginUser.getUser().getId());
+        //根据用户id查询角色信息
+        List<String> roleKeyList = roleService.selectRoleKeyByUserId(loginUser.getUser().getId());
+
+        //获取用户信息
+        User user = loginUser.getUser();
+        UserInfoVo userInfoVo = BeanCopyUtils.copyBean(user, UserInfoVo.class);
+        //封装数据返回
+
+        AdminUserInfoVo adminUserInfoVo = new AdminUserInfoVo(perms,roleKeyList,userInfoVo);
+        return ResponseResult.okResult(adminUserInfoVo);
     }
 
 }
