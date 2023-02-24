f69f1a26dadfa8e5810585d6d11a472b6f410a70
zxwyhzy <2791517764@qq.com>
2023-02-24 17:09:43 +0800

    启动预处理，将文章浏览量存到redis

---
+++ Blog/zy-blog/src/main/java/com/hzy/runner/ViewCountRunner.java
@@ -0,0 +1,41 @@
+package com.hzy.runner;
+
+import com.hzy.domain.entity.Article;
+import com.hzy.mapper.ArticleMapper;
+import com.hzy.utils.RedisCache;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+/**
+ * @title: ViewCountRunner
+ * @Author zxwyhzy
+ * @Date: 2023/2/24 15:41
+ * @Version 1.0
+ */
+@Component
+public class ViewCountRunner implements CommandLineRunner {
+
+    @Autowired
+    private ArticleMapper articleMapper;
+
+    @Autowired
+    private RedisCache redisCache;
+
+    @Override
+    public void run(String... args) throws Exception {
+        //查询博客信息  id  viewCount
+        List<Article> articles = articleMapper.selectList(null);
+        Map<String, Integer> viewCountMap = articles.stream()
+                .collect(Collectors.toMap(article -> article.getId().toString(), article -> {
+                    return article.getViewCount().intValue();//
+                }));
+        //存储到redis中
+        redisCache.setCacheMap("article:viewCount",viewCountMap);
+    }
+}
+
