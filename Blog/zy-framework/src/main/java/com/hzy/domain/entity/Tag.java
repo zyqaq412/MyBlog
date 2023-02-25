568917fe7e3dd3899846ef0e7902026987804949
zxwyhzy <2791517764@qq.com>
2023-02-25 23:23:20 +0800

    给实体类添加@TableField注解实现添加mp字段自动填充

--- Blog/zy-framework/src/main/java/com/hzy/domain/entity/Tag.java
+++ Blog/zy-framework/src/main/java/com/hzy/domain/entity/Tag.java
@@ -3,6 +3,9 @@ package com.hzy.domain.entity;
 import java.util.Date;
 
 import java.io.Serializable;
+
+import com.baomidou.mybatisplus.annotation.FieldFill;
+import com.baomidou.mybatisplus.annotation.TableField;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -25,13 +28,29 @@ public class Tag  {
 
     //标签名
     private String name;
-    
+
+    /**
+     * 创建人的用户id
+     */
+    @TableField(fill = FieldFill.INSERT)
     private Long createBy;
-    
+
+    /**
+     * 创建时间
+     */
+    @TableField(fill = FieldFill.INSERT)
     private Date createTime;
-    
+
+    /**
+     * 更新人
+     */
+    @TableField(fill = FieldFill.INSERT_UPDATE)
     private Long updateBy;
-    
+
+    /**
+     * 更新时间
+     */
+    @TableField(fill = FieldFill.INSERT_UPDATE)
     private Date updateTime;
     //删除标志（0代表未删除，1代表已删除）
     private Integer delFlag;
