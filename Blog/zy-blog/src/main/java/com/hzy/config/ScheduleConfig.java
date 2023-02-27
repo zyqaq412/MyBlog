6d0fbcf9f06e21756800dd50f060eacd7ef80bdb
zxwyhzy <2791517764@qq.com>
2023-02-27 17:48:35 +0800

    定时任务多线程

---
+++ Blog/zy-blog/src/main/java/com/hzy/config/ScheduleConfig.java
@@ -0,0 +1,23 @@
+package com.hzy.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.SchedulingConfigurer;
+import org.springframework.scheduling.config.ScheduledTaskRegistrar;
+
+import java.util.concurrent.Executors;
+
+/**
+ * @title: ScheduleConfig
+ * @Author zxwyhzy
+ * @Date: 2023/2/27 17:47
+ * @Version 1.0
+ */
+@Configuration
+//所有的定时任务都放在一个线程池中，定时任务启动时使用不同都线程。
+public class ScheduleConfig implements SchedulingConfigurer {
+    @Override
+    public void configureTasks(ScheduledTaskRegistrar taskRegistrar) {
+        //设定一个长度10的定时任务线程池
+        taskRegistrar.setScheduler(Executors.newScheduledThreadPool(10));
+    }
+}
